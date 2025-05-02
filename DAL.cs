ff10b69b814d945b9a2419e37edddb35882451a7
strickland77 <agstrickland@wpi.edu>
2025-05-02 15:30:38 -0400

    Separate classes into different files

---
+++ DAL.cs
@@ -0,0 +1,61 @@
+using System.Data;
+
+class DAL
+{
+    internal static MySql.Data.MySqlClient.MySqlConnection Connect()
+    {
+        MySql.Data.MySqlClient.MySqlConnection conn;
+        string myConnectionString;
+
+        myConnectionString = "server=127.0.0.1;port=3306;uid=root;pwd=password;database=ATM";
+
+        conn = new MySql.Data.MySqlClient.MySqlConnection();
+        conn.ConnectionString = myConnectionString;
+        conn.Open();
+
+        return conn;
+    }
+
+    internal static User Login(string login, int pin)
+    {
+        var conn = Connect();
+
+        var cmd = new MySql.Data.MySqlClient.MySqlCommand();
+        cmd.Connection = conn;
+        cmd.CommandType = CommandType.Text;
+        cmd.CommandText = "select * from atm.users where login = @login and pin = @pin";
+        cmd.Parameters.AddWithValue("@login", login);
+        cmd.Parameters.AddWithValue("@pin", pin);
+
+        var reader = cmd.ExecuteReader();
+        while (reader.Read())
+        {
+            string db_login = reader["login"].ToString();
+            int db_pin = Convert.ToInt32(reader["pin"]);
+            string db_name = reader["name"].ToString();
+            double db_balance = Convert.ToDouble(reader["balance"]);
+            int db_account_number = Convert.ToInt32(reader["account_number"]);
+            string db_status = reader["status"].ToString();
+
+            if (db_name == "Admin")
+            {
+                Admin user = new Admin(db_login, db_pin, db_name, db_balance, db_account_number, db_status);
+                return user;
+            }
+            else
+            {
+                Customer user = new Customer(db_login, db_pin, db_name, db_balance, db_account_number, db_status);
+                return user;
+            }
+
+            Console.WriteLine(db_login);
+            Console.WriteLine(db_pin);
+            Console.WriteLine(db_name);
+            Console.WriteLine(db_balance);
+            Console.WriteLine(db_account_number);
+        }
+
+        Console.WriteLine("Found no account matching those credentials...");
+        return null;
+    }
+}
---
+++ User.cs
@@ -0,0 +1,63 @@
+abstract class User
+{
+    private string login;
+    private int pin;
+    private string name;
+    private double balance;
+    private int account_number;
+
+    private string status;
+
+    public User(string input_login, int input_pin, string input_name, double input_balance, int input_account_number, string input_status)
+    {
+        this.login = input_login;
+        this.pin = input_pin;
+        this.name = input_name;
+        this.balance = input_balance;
+        this.account_number = input_account_number;
+        this.status = input_status;
+    }
+
+    public abstract void DisplayMenu();
+
+    public void Exit()
+    {
+        Console.WriteLine("Exiting ATM...");
+        Environment.Exit(0);
+    }
+
+    public int GetAccountNumber()
+    {
+        return this.account_number;
+    }
+
+    public string GetAccountName()
+    {
+        return this.name;
+    }
+
+    public double GetAccountBalance()
+    {
+        return this.balance;
+    }
+
+    public string GetAccountStatus()
+    {
+        return this.status;
+    }
+
+    public string GetAccountLogin()
+    {
+        return this.login;
+    }
+
+    public int GetAccountPin()
+    {
+        return this.pin;
+    }
+
+    public void SetAccountBalance(double input_balance)
+    {
+        this.balance = input_balance;
+    }
+}
